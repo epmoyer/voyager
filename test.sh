a13c29186163340d26f77f7293f7fca20a6ac843
Eric Moyer <eric@lemoncrab.com>
2022-12-16 12:09:36 -0800

    wip: bash

--- test.sh
+++ test.sh
@@ -1,50 +1,34 @@
 #!/bin/zsh
 
 # echo "================================================================================="
-# TARGET_PATH="/Users/eric/Dropbox (Personal)/cab_dbx/code/go/prompt_maker/test1"
+# TARGET_PATH="/Users/eric/Dropbox (Personal)/cab_dbx/code/go/prompt_maker/test1/test2"
 # echo "DUMP:"
-# go run . --dump $TARGET_PATH
+# go run . --dump --powerline $TARGET_PATH
 # echo
-# echo "AS ZSH PROMPT STRING:"
+# echo "AS PROMPT STRING (ZSH, POWERLINE):"
+# go run . --powerline $TARGET_PATH
+# echo
+# echo
+# echo "RENDERED PROMPT STRING (ZSH, POWERLINE):"
+# print -P $(go run . --powerline $TARGET_PATH)
+# echo
+# echo "AS PRINTABLE (ZSH, POWERLINE):"
+# go run . --printable --powerline $TARGET_PATH
+# echo
+# echo
+# echo "AS PROMPT STRING (ZSH, TEXT):"
 # go run . $TARGET_PATH
 # echo
 # echo
-# echo "RENDERED PROMPT STRING:"
+# echo "RENDERED PROMPT STRING (ZSH, POWERLINE):"
 # print -P $(go run . $TARGET_PATH)
 # echo
-# echo
-# echo "AS PRINTABLE:"
+# echo "AS PRINTABLE (TEXT):"
 # go run . --printable $TARGET_PATH
 # echo
 echo "================================================================================="
-TARGET_PATH="/Users/eric/Dropbox (Personal)/cab_dbx/code/go/prompt_maker/test1/test2"
-echo "DUMP:"
-go run . --dump --powerline $TARGET_PATH
+echo "AS PROMPT STRING (BASH, POWERLINE):"
+go run . --shell=bash --powerline $TARGET_PATH
 echo
-echo "AS ZSH PROMPT STRING (POWERLINE):"
-go run . --powerline $TARGET_PATH
-echo
-echo
-echo "RENDERED PROMPT STRING (POWERLINE):"
-print -P $(go run . --powerline $TARGET_PATH)
-echo
-echo "AS PRINTABLE (POWERLINE):"
-go run . --printable --powerline $TARGET_PATH
-echo
-echo
-echo "AS ZSH PROMPT STRING (TEXT):"
-go run . $TARGET_PATH
-echo
-echo
-echo "RENDERED PROMPT STRING (POWERLINE):"
-print -P $(go run . $TARGET_PATH)
-echo
-echo "AS PRINTABLE (TEXT):"
-go run . --printable $TARGET_PATH
 echo
 echo "================================================================================="
-# go run . --dump "/Users/eric/Dropbox (Personal)/cab_dbx/code/go/prompt_maker/"
-# echo XXX
-# print -P $(go run . "/Users/eric/Dropbox (Personal)/cab_dbx/code/go/prompt_maker")
-# echo XXX
-# print -P $(go run . "/Users/eric/Dropbox (Personal)/cab_dbx/code/go/prompt_maker/test1/test2")
