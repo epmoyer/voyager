ed408225fad4e734c8434aa82bd3c2174fb9c385
Eric Moyer <eric@lemoncrab.com>
2023-01-08 19:05:04 -0800

    add VGER_TRUNCATION_START_DEPTH to snippet

--- dist/resources/shell_init_snippet.sh
+++ dist/resources/shell_init_snippet.sh
@@ -4,6 +4,12 @@
 # Allow substitutions and expansions in the prompt
 setopt prompt_subst
 
+# By default Voyager will truncate all but the last directory of your base (pre-git-repo) path.
+# To truncate at a different start depth, uncomment the following.  For example, setting it to
+# 3 will show the final 3 path components.  To show all path components, set it to a large 
+# number (e.g. 1000).
+# export VGER_TRUNCATION_START_DEPTH=3
+
 export VGER_OPT_POWERLINE="--powerline"
 vger_build_prompt() {
   echo $(voyager $VGER_OPT_POWERLINE "$(pwd)")
